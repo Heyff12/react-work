389ccb28b049aef6f54e33b5437720ae4b02818e
Heyff12 <645068564@qq.com>
2019-08-05 16:25:26 +0800

    add test

---
+++ jest.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "setupFiles": ['<rootDir>/tests/setup'],
+    "preset": "ts-jest",
+    "verbose": true,
+    "testRegex": "./src/**/*.spec.(t|j)sx?$",
+
+    "transform": {
+        "^.+\\.(j|t)sx?$": "ts-jest",
+        "^.+\\.(css|png)$": "jest-transform-stub",
+        // "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+        // "^.+\\.tsx?$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+    },
+    "moduleNameMapper": {
+        // "^@/(.*)$": "<rootDir>/src/$1",
+        "\\.(css|less)$":'<rootDiv>/tests/__mock__/styleMock.js',
+        "\\.(jpg|png|jpeg|svg|gif|otf|webp|eot|ttf|woff|woff2|mp4)$":'<rootDiv>/tests/__mock__/fileMock.js',
+    },
+    "testPathIgnorePatterns":['/node_modules'],
+    "moduleFileExtensions": [
+        "js",
+        "json",
+        "ts",
+    ],
+    // 支持源代码中相同的 `@` -> `src` 别名
+    "collectCoverage": true,
+    "collectCoverageFrom": ["./src/**/*.{js,tsx,ts}"],
+    "coverageThreshold":{
+        global:{
+            statements:1,
+            branches:1,
+            functions:1,
+            lines:1,
+        }
+    },
+    "globals": {
+        'ts-jest': {
+            // diagnostics: false,
+            'tsConfig':'<rootDir>/tsconfig.test.json'
+        }
+    }
+    // "testEnvironment": "node"
+    // "coverageReporters": ["html", "text-summary"],//默认格式的测试覆盖率报告
+    // 快照的序列化工具
+    // "snapshotSerializers": ["jest-serializer-vue"],
+}
