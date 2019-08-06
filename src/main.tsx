3aa2844e40709cbb26550bef9c14ab11bc89f404
Heyff12 <645068564@qq.com>
2019-08-06 10:26:55 +0800

    hah

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { ConnectedRouter } from "connected-react-router"
-import "lib-flexible";
-const FastClick = require("fastclick"); // 解决300ms延迟
+import "lib-flexible"
+const FastClick = require("fastclick") // 解决300ms延迟
+// import FastClick = require("fastclick")
 
 import history from "./utils/history"
 import Root from "./Root"
 import store from "./store"
 
-window.FastClick = FastClick;
+window.FastClick = FastClick
 
 import "./styles/app.less"
 
-
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter  history={history}>
-            <Root />
-        </ConnectedRouter>
-    </Provider>, 
-    document.getElementById("app")
-);
-
-
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Root />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("app")
+)
