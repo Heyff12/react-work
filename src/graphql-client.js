337e25fe7da0ccab37493ada0e0cc3de35194767
Heyff12 <645068564@qq.com>
2020-05-20 00:12:26 +0800

    no cache graphql

--- src/graphql-client.js
+++ src/graphql-client.js
@@ -6,5 +6,9 @@ const client = new ApolloClient({
   // uri: 'http://localhost:3001/graphql',
   uri: 'http://182.92.75.69:3001/graphql',
 })
-
+client.defaultOptions = {
+  query: {
+    fetchPolicy: 'no-cache',
+  },
+}
 export default client
